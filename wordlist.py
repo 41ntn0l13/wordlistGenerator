55d72cf0c0690915a6c701065da8d3d76998b769
ghetto23 <92057072+ghetto23@users.noreply.github.com>
2021-12-13 10:37:55 +0000

    Add files via upload

---
+++ wordlist.py
@@ -0,0 +1,31 @@
+import itertools
+import re
+
+print("""
+
+Yontem secin: 
+
+1) Uzunluk ve belirlenin sayiya gore olustur
+2) Belirlenen cumledeki kelimlerle olustur
+
+""")
+yontem = int(input("> "))
+
+if yontem == 1:
+    harf = input("hangi harfler olsun: ")
+    uzunluk = int(input("uzunlugu belirtin: "))
+    for ded in itertools.product(harf, repeat=uzunluk):
+        m = ''.join(ded)
+        f = open("wordlist.txt", "w")
+        f.write(m+"\n")
+        f.close
+
+if yontem == 2:
+    cumle = input("Cumlenizi girin: ")
+    splitmetod = re.split(r"[-;,.'\s]\s*",cumle)
+    dedz = open("wordlist2.txt","w")
+    for qwerty in splitmetod:
+        print(qwerty)
+        dedz.write(qwerty+"\n")
+    dedz.close()
+     
